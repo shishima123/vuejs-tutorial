04a6772d7c547c7f3772799d80ce6b1f069ff8a3
phuoc.nguyen <shishima21@gmail.com>
2020-09-02 23:03:35 +0700

    router-vuex init

---
+++ router-vuex/src/components/company/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+        <div class="form-group">
+            <router-link :to="{name: 'createCompany'}" class="btn btn-success">Create new company</router-link>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Companies list</div>
+            <div class="panel-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Website</th>
+                        <th>Email</th>
+                        <th width="100">&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="company, index in companies">
+                        <td>{{ company.name }}</td>
+                        <td>{{ company.address }}</td>
+                        <td>{{ company.website }}</td>
+                        <td>{{ company.email }}</td>
+                        <td>
+                            <router-link :to="{name: 'editCompany', params: {id: company.id}}" class="btn btn-xs btn-default">
+                                Edit
+                            </router-link>
+                            <a href="#"
+                               class="btn btn-xs btn-danger"
+                               v-on:click="deleteEntry(company.id, index)">
+                                Delete
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
